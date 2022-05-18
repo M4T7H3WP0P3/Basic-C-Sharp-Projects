900ed94d8f993f1bf03460a87f5c363b7c405810
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-05-18 10:50:55 -0600

    Adding entire Basic cs assignment folder

---
+++ Basic cs assignment/Basic cs assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome the Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
