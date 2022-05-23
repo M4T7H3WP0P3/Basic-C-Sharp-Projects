04f32d585703253d693c8d4185be154f67475f9e
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-05-23 13:40:00 -0600

    Upload Loop Assignment

---
+++ LoopAssignment/LoopAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int counter = 10;
+            do
+            {
+                Console.WriteLine(counter + "\t" + Math.Pow(counter, 2));
+                counter--;
+            }
+            while (counter > 5);
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+    }
+}
