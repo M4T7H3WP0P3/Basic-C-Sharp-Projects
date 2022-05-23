04e7e1a686a74e6ca3ee78633ba4f1e59ede2ee4
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-05-23 12:34:54 -0600

    Upload String Assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string dateString = DateTime.Today.ToShortDateString();
+            string timeString = DateTime.Now.ToString("hh:mm tt");
+
+            Console.WriteLine("What is your name?");
+            userName = Console.ReadLine();
+
+            string str = "Hello " + userName + ". Today is " + dateString + " and it is " + timeString + "." ;
+            Console.WriteLine(str);
+                        
+            string welcome = userName.ToUpper() + ", How are you today?";
+            
+            Console.WriteLine(welcome);
+
+            StringBuilder sb = new StringBuilder(userName);
+
+            sb.Append(" I hope");
+            sb.Append(" you have a ");
+            sb.Append("great day!");
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
+        }
+        
+    }
+}
