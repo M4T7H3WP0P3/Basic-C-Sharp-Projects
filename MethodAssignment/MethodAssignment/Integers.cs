4e7b00602a8b8821c777f47917e9a94e41c1dee4
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-05-30 09:48:07 -0600

    Add files via upload

---
+++ MethodAssignment/MethodAssignment/Integers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Integers
+    {
+        public int MultiplyTwoNumbers(int x)
+        {
+            return x * 5;
+        }
+
+        public int AddTwoNumbers(int x)
+        {
+            return x + 10;
+        }
+
+        public int DivideTwoNumbers(int x)
+        {
+            return 50 / x;
+        }
+        //public int AddTwoNumbers(int userInput, int num1 = 5)
+        //{
+        //    int addResult;
+            
+        //    addResult = num1 + userInput;
+
+        //    return addResult;
+        //}
+        //public int MultiplyTwoNumbers(int num2, int userInput)
+        //{
+        //    int multiplyResult;
+
+        //    multiplyResult = num2 * userInput;
+
+        //    return multiplyResult;
+        //}
+        //public int DivideTwoNumbers(int num3, int userInput)
+        //{
+        //    int divideResult;
+
+        //    divideResult = num3 / userInput;
+
+        //    return divideResult;
+        //}
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please pick a number that we will use in our math operations");
+            int userInp = Convert.ToInt32(Console.ReadLine());
+
+            Integers obj1 = new Integers();
+
+            Console.WriteLine("Your number multiplied by 5 is " + obj1.MultiplyTwoNumbers(userInp));
+            Console.WriteLine("Your number added to 10 is " + obj1.AddTwoNumbers(userInp));
+            Console.WriteLine("50 divided by your number is " + obj1.DivideTwoNumbers(userInp));
+            Console.ReadLine();
+            //int num1 = 5;
+            //int num2 = 8;
+            //int num3 = 50;
+
+            //Console.WriteLine("Please pick a number that we will use in our math operations");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //Integers.AddTwoNumbers();
+
+        }                    
+        
+    }
+}
