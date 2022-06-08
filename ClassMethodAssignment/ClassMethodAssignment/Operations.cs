5e65701b275d12d37bea98472319de23d1203f83
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-06-08 13:58:21 -0600

    Upload ClassMethodAssignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/Operations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Operations
+    {
+        public void Operation(out int x, int y)
+        {
+            x = 2;
+
+            int result = y / x;
+
+            Console.WriteLine("Your number will be divided by " + x);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public void Operation(out int x, out int z, int y)
+        {
+            x = 5;
+            z = 10;
+
+            int result1 = x * y;
+
+            int result2 = y + z;
+
+            Console.WriteLine("You number will be multiplied by " + x);
+            Console.WriteLine(result1);
+
+            Console.WriteLine("Your number will be added to " + z);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+     static class Program
+     {
+        static void Main(string[] args)
+        {
+            Operations operation = new Operations();
+
+            Console.WriteLine("Please enter a number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            int x;
+            operation.Operation(out x , y: input1 );
+
+
+            Console.WriteLine("Please choose a another number");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            
+            int z;
+            operation.Operation(out x, out z, y: input2);
+
+        }
+     } 
+}
+
+
+//Class Method Assignment
+//Perform these actions and create a console app that includes the following:
+
+//Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+//In the Main() method, instantiate that class.
+//Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+//Create a method with output parameters.
+//Overload a method.
+//Declare a class to be static.
+
+//Operations operation = new Operations();
+
+//Console.WriteLine("Please enter a number: ");
+//int userInput = Convert.ToInt32(Console.ReadLine());
+
+//operation.Operation(userInput);
