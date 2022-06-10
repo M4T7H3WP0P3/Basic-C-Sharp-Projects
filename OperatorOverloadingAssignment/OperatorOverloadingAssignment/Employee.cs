0fb032033b37edf1d94203b3298c06120a40c4d6
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-06-10 10:21:45 -0600

    Upload Operator Overloading Assignment

---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+
+        public Employee(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        //public void Display()
+        //{
+        //    Console.WriteLine($"ID: {id}");
+        //    Console.ReadLine();
+        //}
+
+
+
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            if (e1.id == e2.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            if (e1.id != e2.id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }    
+        }
+    }      
+
+}
+//public static List<Employee> Employees { get; set; }
+//public int ID { get; set; }
+//public string firstName { get; set; }
+//public string lastName { get; set; }
+
+//public virtual void ListEmployees()
+//{
+//    foreach (Employee employee in Employees)
+//    {
+//        Console.WriteLine(employee);
+
+//    }
+//}
+//public static Program operator+ (Employee employee)
+//{
+//    employee.Employees.Add(employee);
+
+//}
---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "Matthew");
+
+            Employee e2 = new Employee(2, "John");
+            //e1.Display();
+
+            if (e1 == e2)
+            {
+                Console.WriteLine(e1 == e2);
+                Console.ReadLine();
+            }
+
+
+
+
+            
+
+            
+
+
+
+
+
+
+        }
+    }
+}
+//Perform these actions and create a console app that includes the following:
+
+//Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+
+//Employee employee = new Employee();
+//employee.ID = new List<Employees>;
+//Employee employees = new Employees();
+//employee.firstName = "Matthew";
+//employee.lastName = "Pope";
+//employee.ID = 1;
+
+//employee.firstName = "John";
+//employee.lastName = "Cena";
+//employee.ID = 2;
+
+//Console.WriteLine();
