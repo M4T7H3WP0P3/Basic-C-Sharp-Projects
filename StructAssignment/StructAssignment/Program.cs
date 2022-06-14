e7e3326a13d78726082f4a93e1d7bc1c134434ba
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-06-14 09:51:02 -0600

    Upload Structs Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 2.3m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    struct Number
+    {
+        public decimal Amount;
+    }
+}
+
+//Perform these actions and create a console app that includes the following:
+
+//Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+//In the Main() method, create an object of data type Number and assign an amount to it.
+//Print this amount to the console.
+
+//A class is a reference type and a struct is a value type that can't inherit
