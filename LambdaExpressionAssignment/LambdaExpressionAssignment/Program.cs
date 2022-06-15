dbc31da9d52ea7391b499045b7199a58ba2e23ae
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-06-15 12:50:21 -0600

    Add files via upload

--- LambdaExpressionAssignment/LambdaExpressionAssignment/Program.cs
+++ LambdaExpressionAssignment/LambdaExpressionAssignment/Program.cs
@@ -6,99 +6,80 @@ using System.Threading.Tasks;
 
 namespace LambdaExpressionAssignment
 {
-    class Program
+    public class Program
     {
+        public class Employee
+        {
+            public string firstName;
+            public string lastName;
+            public int empId;
+
+            public Employee(string xfName, string xlName, int xID)
+            {
+                firstName = xfName;
+                lastName = xlName;
+                empId = xID;
+            }
+        }
+
         static void Main(string[] args)
         {
-            List<Employees> employees = new List<Employees>();
+            //Perform these actions and create a console app that includes the following:
 
-            employees.Add(new Employees()
-            {
-                Id = 1,
-                firstName = "Matthew",
-                lastName = "Pope"
-            });
+            //In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name "Joe".
 
-            employees.Add(new Employees()
-            {
-                Id = 2,
-                firstName = "John",
-                lastName = "Cena"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 3,
-                firstName = "Joe",
-                lastName = "Mama"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 4,
-                firstName = "Joe",
-                lastName = "Sakic"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 5,
-                firstName = "Jesse",
-                lastName = "Johnson"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 6, 
-                firstName = "Brett",
-                lastName = "Favre"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 7, 
-                firstName = "Barack", 
-                lastName = "Obama"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 8,
-                firstName = "Donald",
-                lastName = "Trump"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 9, 
-                firstName = "Naruto",
-                lastName = "Uzumaki"
-            });
-            employees.Add(new Employees()
-            {
-                Id = 10, 
-                firstName = "Sasuke",
-                lastName = "Uchiha"
-            });
-            
-            foreach (Employees emp in employees)
-            {
-                if (emp.firstName.Contains("Joe"))
-                {
-                    List<string> newList = new List<string>();
-                    newList.Add(emp.firstName);
-                    Console.WriteLine(newList);
-                }
-            }
-            Console.ReadLine();
-            //foreach (var value in employees)
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Matthew ", "Pope", 1));
+            employees.Add(new Employee("Allen ", "Iverson", 2));
+            employees.Add(new Employee("Jesse ", "Johnson", 3));
+            employees.Add(new Employee("Israel ", "Adensanya", 4));
+            employees.Add(new Employee("Kamaru ", "Usman", 5));
+            employees.Add(new Employee("Barack ", "Obama", 6));
+            employees.Add(new Employee("Donald ", "Trump", 7));
+            employees.Add(new Employee("Joe", "Sakic", 8));
+            employees.Add(new Employee("Joe", "Mama", 9));
+            employees.Add(new Employee("John ", "Cena", 10));
+
+            //Using a foreach loop, create a new list of all employees with the first name "Joe".
+
+            //List<string> joeList = new List<string>();
+
+            //foreach(Employee employee in employees)
             //{
-            //    var empList = value.firstName;
-            //    if ()
-            //}
-            
+            //    if (employee.firstName.Contains("Joe"))
+            //    {
+            //        joeList.Add(employee.firstName);
+            //        Console.WriteLine(employee.firstName + "" +employee.lastName); 
+            //    }
+            //}           
+            //Console.ReadLine();
 
+            //Perform the same action again, but this time with a lambda expression.
+
+            //List<Employee> lambList = employees.Where(e => e.firstName == "Joe").ToList();
+
+
+
+            //foreach(Employee employee in lambList)
+            //{
+            //    Console.WriteLine(employee.firstName + " " + employee.lastName);
+            //}
+
+            //Using a lambda expression, make a list of all employees with an Id number greater than 5.
+
+            List<Employee> empId = employees.Where(x => x.empId > 5).ToList();
+
+            foreach(Employee employee in empId)
+            {
+                Console.WriteLine(employee.firstName + " " + employee.lastName + " " + employee.empId);
+            }
+
+            Console.ReadLine();
         }
     }
 }
 
-//Perform these actions and create a console app that includes the following:
 
-//In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name "Joe".
-//Using a foreach loop, create a new list of all employees with the first name "Joe".
-//Perform the same action again, but this time with a lambda expression.
-//Using a lambda expression, make a list of all employees with an Id number greater than 5.
-//Upload your code to GitHub and paste the link in the space below:
+//Notes
+//we can't put the list name in the WriteLine() because it won't print off the contents, just the list object itself.
+
