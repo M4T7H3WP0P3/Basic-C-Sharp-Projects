e4ad66a0aefd46fab061ef3df6da9aad7f41a663
M4T7H3WP0P3 <matthew_pope_91@hotmail.com>
2022-06-22 17:24:02 -0600

    Upload MVC Sample Application

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\matth\Logs\log2.txt", text);
+
+            Random rnd = new Random(10);
+            int num = rnd.Next();
+
+            if (num > 20000)
+            {
+                return View("About");
+            }
+
+            //ViewBag.RandomNumber = num; "catch all"
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
