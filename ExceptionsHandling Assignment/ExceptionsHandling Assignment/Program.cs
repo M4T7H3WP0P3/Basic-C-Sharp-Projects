81ad19d4b550efbc7b28619a2fedb2a30d2a0faf
M4T7H3WP0P3 <matthew_pope_91@hotmail.com>
2022-06-25 11:35:53 -0600

    Upload Exceptions Handling Assignment

---
+++ ExceptionsHandling Assignment/ExceptionsHandling Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsHandling_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter you age:");
+
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0 || age < 0)
+                {
+                    throw new OverflowException();
+                }
+                int birthYear = DateTime.Now.Year - age;
+                Console.WriteLine("You were born in {0}", birthYear);
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Your age can not be zero, or less than zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//Perform these actions and create a console app that includes the following:
+
+//Ask the user for their age.
+//Display the year the user was born.
+//Exceptions must be handled using “try/catch.”
+//Display appropriate error messages if the user enters zero or negative numbers.
+//Display a general message if an exception was caused by anything else.
