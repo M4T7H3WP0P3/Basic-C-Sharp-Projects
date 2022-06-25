9b661a53485a0bc5c84dd50a82c78b7a391488f8
M4T7H3WP0P3 <matthew_pope_91@hotmail.com>
2022-06-25 11:03:19 -0600

    Upload Final Assignment

---
+++ CodeFirstAssignment/CodeFirstAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                var student = new Student() { FirstName = "Matthew", LastName = "Pope" };
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual List<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
